f9d73149d1f0aaf2e0acab18ae5aede65abeb5ae
Kubiczeek <jakubakubek@gmail.com>
2023-04-01 22:40:28 +0200

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="m-4 p-4 text-3xl font-bold text-red-500 underline">Hello world!</h1>
